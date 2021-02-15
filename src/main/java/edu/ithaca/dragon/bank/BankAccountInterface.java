8f2c8b3692eac392b9ff8494402de354565dc315
eostendarp <eostendarp@ithaca.edu>
2021-02-15 09:21:00 -0500

    add isAmountValid

--- src/main/java/edu/ithaca/dragon/bank/BankAccountInterface.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccountInterface.java
@@ -1,5 +1,7 @@
 package edu.ithaca.dragon.bank;
 
+import java.math.BigDecimal;
+
 public interface BankAccountInterface {
 
     public double getBalance();
@@ -11,6 +13,21 @@ public interface BankAccountInterface {
             return false;
         }
         else {
+            return true;
+        }
+    }
+
+    /**
+     * Returns true if amount has 2 or fewer decimal places and is non-negative, false otherwise
+     * @param amount value to check
+     * @return true if amount is valid, false otherwise
+     */
+    public static boolean isAmountValid(double amount) {
+        if (amount < 0) {
+            return false;
+        } else if (BigDecimal.valueOf(amount).scale() > 2) {
+            return false;
+        } else {
             return true;
         }
     }
