8a017e60abe3d395eafb2bf42bf7cd05f911d7ce
eostendarp <eostendarp@ithaca.edu>
2021-02-25 15:24:06 -0500

    SavingsAccount method headers

--- src/main/java/edu/ithaca/dragon/bank/SavingsAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/SavingsAccount.java
@@ -1,5 +1,10 @@
 package edu.ithaca.dragon.bank;
 
 public class SavingsAccount extends Account {
-    
+    private double interestRate;
+
+    public SavingsAccount(double balance, double interestRate) {
+        super(balance);
+        this.interestRate = interestRate;
+    }
 }
