113b0c90464a2e01561639d94850b49f27e8ff1f
eostendarp <eostendarp@ithaca.edu>
2021-02-25 14:54:52 -0500

    admin method headers

--- src/main/java/edu/ithaca/dragon/bank/Admin.java
+++ src/main/java/edu/ithaca/dragon/bank/Admin.java
@@ -1,5 +1,27 @@
 package edu.ithaca.dragon.bank;
 
 public class Admin {
-    
+    private BankController bankController;
+
+    public Admin(BankController bankController) {
+        this.bankController = bankController;
+    }
+
+    public double checkOverallAmount() {
+        // TODO implement checkOverallAmount
+        return 0;
+    }
+
+    public String checkSuspiciousActivity() {
+        // TODO implement checkSuspiciousActivity
+        return "";
+    }
+
+    public void freeze(int accountId) {
+        // TODO implement freeze
+    }
+
+    public void unfreeze(int accountId) {
+        // TODO implement unfreeze
+    }
 }
