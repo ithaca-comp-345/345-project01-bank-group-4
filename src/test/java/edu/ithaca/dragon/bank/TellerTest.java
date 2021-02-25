0e8fdd4d8fef6a23a7f11a3292fb89eebb747182
eostendarp <eostendarp@ithaca.edu>
2021-02-25 17:18:47 -0500

    TellerTest method headers

--- src/test/java/edu/ithaca/dragon/bank/TellerTest.java
+++ src/test/java/edu/ithaca/dragon/bank/TellerTest.java
@@ -1,5 +1,61 @@
 package edu.ithaca.dragon.bank;
 
+import org.junit.jupiter.api.Test;
+
 public class TellerTest {
-    
+    @Test
+    public void confirmCredentialsTest() {
+
+    }
+
+    @Test
+    public void checkBalanceTest() {
+
+    }
+
+    @Test
+    public void withdrawTest() {
+
+    }
+
+    @Test
+    public void depositTest() {
+
+    }
+
+    @Test
+    public void transferTest() {
+
+    }
+
+    @Test
+    public void retrieveTransactionHistoryTest() {
+
+    }
+
+    // ATM specific tests
+
+    @Test
+    public void ATMConstructorTest() {
+
+    }
+
+
+    // HumanTeller specific tests
+
+    @Test
+    public void humanTellerConstructorTest() {
+
+    }
+
+
+    @Test
+    public void createAccountTest() {
+
+    }
+
+    @Test
+    public void closeAccountTest() {
+
+    }
 }
