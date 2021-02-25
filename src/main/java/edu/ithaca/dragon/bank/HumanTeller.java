c826aebe3e155557156e4f9ef7141c60034e2542
eostendarp <eostendarp@ithaca.edu>
2021-02-25 15:01:45 -0500

    HumanTeller method headers

--- src/main/java/edu/ithaca/dragon/bank/HumanTeller.java
+++ src/main/java/edu/ithaca/dragon/bank/HumanTeller.java
@@ -1,5 +1,17 @@
 package edu.ithaca.dragon.bank;
 
 public class HumanTeller extends Teller {
+
+    public HumanTeller(BankController bankController) {
+        super(bankController);
+    }
     
+    public int createAccount(char accountType) {
+        // TODO implement createAccount
+        return 0;
+    }
+
+    public void closeAccount() {
+        // TODO implement closeAccount
+    }
 }
