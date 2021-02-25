e719bc02924f591f44a55e983dff4540df0bc5c1
eostendarp <eostendarp@ithaca.edu>
2021-02-25 15:21:39 -0500

    CentralBank method headers

--- src/main/java/edu/ithaca/dragon/bank/CentralBank.java
+++ src/main/java/edu/ithaca/dragon/bank/CentralBank.java
@@ -1,5 +1,29 @@
 package edu.ithaca.dragon.bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CentralBank {
+    private BankController bankController;
+    private Admin admin;
+    private List<Teller> tellers;
     
+    public CentralBank(BankController bankController, Admin admin) {
+        this.bankController = bankController;
+        this.admin = admin;
+        tellers = new ArrayList<>();
+        // TODO add defaul teller
+    }
+
+    public BankController getBankController() {
+        return bankController;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public List<Teller> getTellers() {
+        return tellers;
+    }
 }
