4df7ca5189dac975209b47242eab16fe8eb5180c
eostendarp <eostendarp@ithaca.edu>
2021-02-25 14:51:35 -0500

    Account method headers

--- src/main/java/edu/ithaca/dragon/bank/Account.java
+++ src/main/java/edu/ithaca/dragon/bank/Account.java
@@ -1,5 +1,37 @@
 package edu.ithaca.dragon.bank;
 
 public abstract class Account {
-    
+    private double balance;
+    private boolean suspicious;
+    private boolean frozen;
+
+    public Account(double balance) {
+        this.balance = balance;
+        suspicious = false;
+        frozen = false;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean isSuspicious() {
+        return suspicious;
+    }
+
+    public boolean isFrozen() {
+        return frozen;
+    }
+
+    public void withdraw(double amount) {
+        // TODO implement withdraw
+    }
+
+    public void deposit(double amount) {
+        // TODO implement deposit
+    }
+
+    public void transfer(Account account, double amount) {
+        // TODO implement transfer
+    }
 }
