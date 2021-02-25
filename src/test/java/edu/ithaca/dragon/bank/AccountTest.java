22ce168757230b551f55c11b959f494bd10c4b83
eostendarp <eostendarp@ithaca.edu>
2021-02-25 17:04:09 -0500

    AccountTest method headers

--- src/test/java/edu/ithaca/dragon/bank/AccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AccountTest.java
@@ -1,5 +1,25 @@
 package edu.ithaca.dragon.bank;
 
+import org.junit.jupiter.api.Test;
+
 public class AccountTest {
-    
+    @Test
+    public void ConstructorTest() {
+
+    }
+
+    @Test
+    public void WithdrawTest() {
+
+    }
+
+    @Test
+    public void DepositTest() {
+
+    }
+
+    @Test
+    public void TransferTest() {
+
+    }
 }
