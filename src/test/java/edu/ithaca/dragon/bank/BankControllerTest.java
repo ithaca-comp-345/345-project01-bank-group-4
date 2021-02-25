8fd45599b47050ead75c459469f2e7ec80acaa22
eostendarp <eostendarp@ithaca.edu>
2021-02-25 17:10:45 -0500

    BankControllerTest method headers

--- src/test/java/edu/ithaca/dragon/bank/BankControllerTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankControllerTest.java
@@ -1,5 +1,70 @@
 package edu.ithaca.dragon.bank;
 
+import org.junit.jupiter.api.Test;
+
 public class BankControllerTest {
-    
+    @Test
+    public void constructorTest() {
+
+    }
+
+    @Test
+    public void retrieveAccountTest() {
+        
+    }
+
+    @Test
+    public void checkBalanceTest() {
+        
+    }
+
+    @Test
+    public void withdrawTest() {
+        
+    }
+
+    @Test
+    public void depositTest() {
+        
+    }
+
+    @Test
+    public void transferTest() {
+        
+    }
+
+    @Test
+    public void retrieveTransactionHistoryTest() {
+        
+    }
+
+    @Test
+    public void createAccountTest() {
+        
+    }
+
+    @Test
+    public void closeAccountTest() {
+        
+    }
+
+    @Test
+    public void checkOverallAmountTest() {
+        
+    }
+
+    @Test
+    public void checkSuspiciousActivityTest() {
+        
+    }
+
+    @Test
+    public void freezeTest() {
+        
+    }
+
+    @Test
+    public void unfreezeTest() {
+        
+    }
 }
