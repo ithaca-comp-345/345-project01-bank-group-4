b5f98bca0c27b643b2ffc7de97e30e0f235e5c02
eostendarp <eostendarp@ithaca.edu>
2021-02-25 14:27:10 -0500

    start fresh

--- src/main/java/edu/ithaca/dragon/bank/InsufficientFundsException.java
+++
@@ -1,9 +0,0 @@
-package edu.ithaca.dragon.bank;
-
-public class InsufficientFundsException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public InsufficientFundsException(String s) {
-        super(s);
-    }
-}
