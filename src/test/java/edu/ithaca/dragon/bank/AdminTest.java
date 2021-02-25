865d341194581b72d173e7f46be02af5aaf22105
eostendarp <eostendarp@ithaca.edu>
2021-02-25 17:06:11 -0500

    AdminTest method headers

--- src/test/java/edu/ithaca/dragon/bank/AdminTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminTest.java
@@ -1,5 +1,30 @@
 package edu.ithaca.dragon.bank;
 
+import org.junit.jupiter.api.Test;
+
 public class AdminTest {
-    
+    @Test
+    public void constructorTest() {
+
+    }
+
+    @Test
+    public void checkOverallAmountTest() {
+
+    }
+
+    @Test
+    public void checkSuspiciousActivityTest() {
+
+    }
+
+    @Test
+    public void freezeTest() {
+
+    }
+
+    @Test
+    public void unfreezeTest() {
+
+    }
 }
