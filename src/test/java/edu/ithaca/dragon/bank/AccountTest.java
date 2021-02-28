ea18d2c34b6aaf7b43f1106e851413a51c2df72b
eostendarp <eostendarp@ithaca.edu>
2021-02-27 20:08:31 -0500

    savingsConstructorTest

--- src/test/java/edu/ithaca/dragon/bank/AccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AccountTest.java
@@ -51,7 +51,7 @@ public class AccountTest {
 
         CheckingAccount checkingAccount;
 
-        // VALID BALANCES
+        // VALID
 
         checkingAccount = new CheckingAccount(200);
         assertEquals(200, checkingAccount.getBalance(), delta);
@@ -74,7 +74,7 @@ public class AccountTest {
         assertEquals(false, checkingAccount.isFrozen());
 
 
-        // INVALID BALANCES
+        // INVALID
 
         // balance negative
         assertThrows(IllegalArgumentException.class, () -> new CheckingAccount(-200));
@@ -94,6 +94,71 @@ public class AccountTest {
 
     @Test
     public void savingsConstructorTest() {
+        double delta = .001;
+
+        SavingsAccount savingsAccount;
+
+        // VALID
+
+        savingsAccount = new SavingsAccount(200, .02);
+        assertEquals(200, savingsAccount.getBalance(), delta);
+        assertEquals(.02, savingsAccount.getInterestRate(), delta);
+        assertEquals("", savingsAccount.getTransactionHistory());
+        assertEquals(false, savingsAccount.isSuspicious());
+        assertEquals(false, savingsAccount.isFrozen());
+
+        // balance .01 -boundary case
+        savingsAccount = new SavingsAccount(.01, .04);
+        assertEquals(.01, savingsAccount.getBalance(), delta);
+        assertEquals(.04, savingsAccount.getInterestRate(), delta);
+        assertEquals("", savingsAccount.getTransactionHistory());
+        assertEquals(false, savingsAccount.isSuspicious());
+        assertEquals(false, savingsAccount.isFrozen());
+
+        // balance 0 -boundary case
+        savingsAccount = new SavingsAccount(0, .03);
+        assertEquals(0, savingsAccount.getBalance(), delta);
+        assertEquals(.03, savingsAccount.getInterestRate(), delta);
+        assertEquals("", savingsAccount.getTransactionHistory());
+        assertEquals(false, savingsAccount.isSuspicious());
+        assertEquals(false, savingsAccount.isFrozen());
+
+        // interestRate .01 -boundary case
+        savingsAccount = new SavingsAccount(200, .01);
+        assertEquals(200, savingsAccount.getBalance(), delta);
+        assertEquals(.01, savingsAccount.getInterestRate(), delta);
+        assertEquals("", savingsAccount.getTransactionHistory());
+        assertEquals(false, savingsAccount.isSuspicious());
+        assertEquals(false, savingsAccount.isFrozen());
+
+        // interestRate 0 -boundary case
+        savingsAccount = new SavingsAccount(200, 0);
+        assertEquals(200, savingsAccount.getBalance(), delta);
+        assertEquals(0, savingsAccount.getInterestRate(), delta);
+        assertEquals("", savingsAccount.getTransactionHistory());
+        assertEquals(false, savingsAccount.isSuspicious());
+        assertEquals(false, savingsAccount.isFrozen());
+
+
+        // INVALID
+
+        // balance negative
+        assertThrows(IllegalArgumentException.class, () -> new SavingsAccount(-200, .02));
+
+        // balance -.01 -boundary case
+        assertThrows(IllegalArgumentException.class, () -> new SavingsAccount(-.01, .03));
+
+        // balance decimal places > 2
+        assertThrows(IllegalArgumentException.class, () -> new SavingsAccount(1.24816, .04));
+
+        // balance decimal place 3 -boundary case
+        assertThrows(IllegalArgumentException.class, () -> new SavingsAccount(3.141, .05));
+
+        // interestRate negative
+        assertThrows(IllegalArgumentException.class, () -> new SavingsAccount(200, -.02));
+
+        // interestRate -.01 -boundary case
+        assertThrows(IllegalArgumentException.class, () -> new SavingsAccount(200, -.01));
     }
 
     @Test
