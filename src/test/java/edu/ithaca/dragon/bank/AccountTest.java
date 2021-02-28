ccbfb496cb4b12498b8fd877943aade8aea27a76
eostendarp <eostendarp@ithaca.edu>
2021-02-27 19:54:18 -0500

    checkingConstructorTest

--- src/test/java/edu/ithaca/dragon/bank/AccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AccountTest.java
@@ -1,5 +1,8 @@
 package edu.ithaca.dragon.bank;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class AccountTest { 
@@ -44,10 +47,41 @@ public class AccountTest {
 
     @Test
     public void checkingConstructorTest() {
+        double delta = .001;
+
+        CheckingAccount checkingAccount;
+
+        // VALID BALANCES
+
+        checkingAccount = new CheckingAccount(200);
+        assertEquals(200, checkingAccount.getBalance(), delta);
+
+        // balance 0 -boundary case
+        checkingAccount = new CheckingAccount(0);
+        assertEquals(0, checkingAccount.getBalance(), delta);
+
+        // balance .01 -boundary case
+        checkingAccount = new CheckingAccount(.01);
+        assertEquals(.01, checkingAccount.getBalance(), delta);
+
+
+        // INVALID BALANCES
+
+        // balance negative
+        assertThrows(IllegalArgumentException.class, () -> new CheckingAccount(-200));
+
+        // balance -.01 -boundary case
+        assertThrows(IllegalArgumentException.class, () -> new CheckingAccount(-.01));
+
+        // balance decimal places > 2
+        assertThrows(IllegalArgumentException.class, () -> new CheckingAccount(1.24816));
+
+        // balance decimal place 3 -boundary case
+        assertThrows(IllegalArgumentException.class, () -> new CheckingAccount(3.141));
     }
 
 
-    // SavingsAccout secific tests
+    // SavingsAccout specific tests
 
     @Test
     public void savingsConstructorTest() {
