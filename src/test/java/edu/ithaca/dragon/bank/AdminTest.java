befd3b5f7df7d9eb8356918fc59f743191c7485e
eostendarp <eostendarp@ithaca.edu>
2021-02-28 10:30:38 -0500

    constructorTest

--- src/test/java/edu/ithaca/dragon/bank/AdminTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminTest.java
@@ -1,10 +1,24 @@
 package edu.ithaca.dragon.bank;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class AdminTest {
     @Test
     public void constructorTest() {
+        Admin admin;
+        BankController bankController;
+
+        // VALID
+
+        admin = new Admin(bankController = new BankController());
+        assertEquals(bankController, admin.getBankController());
+
+        // INVALID
+
+        assertThrows(NullPointerException.class, () -> new Admin(null));
     }
  
     @Test
