292fc7ad0398879d2c3c756da123c8645acfe091
Bobolicious cool kid <bbradley@ithaca.edu>
2021-03-01 02:14:40 -0500

    changed branch and system test name

---
+++ src/test/java/edu/ithaca/dragon/bank/UserSystemTest.java
@@ -0,0 +1,12 @@
+package edu.ithaca.dragon.bank;
+import org.junit.jupiter.api.Test;
+
+
+public class DepositSystemTest {
+    @Test
+    public void accountRunTest() {
+        Account testAccount = new Acc
+        
+    }
+    
+}
