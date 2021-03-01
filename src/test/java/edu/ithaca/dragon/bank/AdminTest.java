8fe0e452f482904f4fa414cbba4e81ecb111a8a0
eostendarp <eostendarp@ithaca.edu>
2021-02-28 23:59:28 -0500

    update checkSuspiciousActivityTest to ignore order of suspicious accounts, and write setSuspiciousTest

--- src/test/java/edu/ithaca/dragon/bank/AdminTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminTest.java
@@ -2,6 +2,7 @@ package edu.ithaca.dragon.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -62,16 +63,41 @@ public class AdminTest {
         bankController.setSuspicious(accountId1, true);
         assertEquals("Suspicious Accounts: " + accountId1 + ", ", admin.checkSuspiciousActivity());
 
+
         // multiple accounts
         bankController.createSavings(0, .5);
         accountId2 = bankController.createChecking(100);
         bankController.setSuspicious(accountId2, true);
-        assertEquals("Suspicious Accounts: " + accountId1 + ", " + accountId2 + ", ", admin.checkSuspiciousActivity());
+        String suspiciousActivity = admin.checkSuspiciousActivity();
+        assertTrue(suspiciousActivity.contains(accountId1 + "") && suspiciousActivity.contains(accountId2 + ""));
     }
  
     // System Test
     @Test
     public void setSuspiciousTest() {
+        BankController bankController = new BankController();
+        Admin admin = new Admin(bankController);
+
+        int accountId1, accountId2;
+
+        // no accounts -boundary case
+        assertEquals("Suspicious Accounts: ", admin.checkSuspiciousActivity());
+
+        // no suspicious accounts -boudary case
+        bankController.createSavings(0, .04);
+        assertEquals("Suspicious Accounts: ", admin.checkSuspiciousActivity());
+
+        // 1 suspicious account
+        accountId1 = bankController.createChecking(200);
+        admin.setSuspicious(accountId1, true);
+        assertEquals("Suspicious Accounts: " + accountId1 + ", ", admin.checkSuspiciousActivity());
+
+        // multiple accounts
+        bankController.createSavings(0, .5);
+        accountId2 = bankController.createChecking(100);
+        admin.setSuspicious(accountId2, true);
+        String suspiciousActivity = admin.checkSuspiciousActivity();
+        assertTrue(suspiciousActivity.contains(accountId1 + "") && suspiciousActivity.contains(accountId2 + ""));
     }
  
     // System Test
